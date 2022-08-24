6f49527727d0a6606e8d1aab1af4ef8be43ad761
aryantisiregar <aryantisiregar2010@gmail.com>
2022-08-24 16:03:47 +0700

    develop change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Nana!");
-        System.out.println("Hello, Develop!");
+        System.out.println("Hello, Develop Changes!");
         System.out.println("Hello, feature!");
     }
 }
